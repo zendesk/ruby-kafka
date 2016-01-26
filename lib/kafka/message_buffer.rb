25b430a6636ea04fcf4f105d02dccf0ce4cbab25
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-01-26 10:45:49 +0100

    Refactor message buffering
    - Introduce a MessageBuffer class that manages per-partition buffers;
    - remove the Message and MessageSet classes; and
    - move code from Producer to MessageSet.
    
    x

---
+++ lib/kafka/message_buffer.rb
@@ -0,0 +1,42 @@
+module Kafka
+
+  # Buffers messages for specific topics/partitons.
+  class MessageBuffer
+    include Enumerable
+
+    def initialize
+      @buffer = {}
+    end
+
+    def write(message, topic:, partition:)
+      buffer_for(topic, partition) << message
+    end
+
+    def concat(messages, topic:, partition:)
+      buffer_for(topic, partition).concat(messages)
+    end
+
+    def to_h
+      @buffer
+    end
+
+    def each
+      @buffer.each do |topic, messages_for_topic|
+        messages_for_topic.each do |partition, messages_for_partition|
+          yield topic, partition, messages_for_partition
+        end
+      end
+    end
+
+    def clear
+      @buffer = {}
+    end
+
+    private
+
+    def buffer_for(topic, partition)
+      @buffer[topic] ||= {}
+      @buffer[topic][partition] ||= []
+    end
+  end
+end
