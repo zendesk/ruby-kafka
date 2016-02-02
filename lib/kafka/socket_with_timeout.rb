ef0da12d0c924f69a1e40582e13a27ab4bcab663
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-02-02 13:49:26 +0100

    Move socket logic into SocketWithTimeout

--- lib/kafka/socket_with_timeout.rb
+++ lib/kafka/socket_with_timeout.rb
@@ -4,7 +4,7 @@ module Kafka
 
   # Opens sockets in a non-blocking fashion, ensuring that we're not stalling
   # for long periods of time waiting for a connection to open.
-  module SocketWithTimeout
+  class SocketWithTimeout
 
     # Opens a socket.
     #
@@ -12,37 +12,59 @@ module Kafka
     # @param port [Integer]
     # @param timeout [Integer] the connection timeout, in seconds.
     # @return [TCPSocket] the open socket.
-    def self.open(host, port, timeout: nil)
+    def initialize(host, port, timeout: nil)
       addr = Socket.getaddrinfo(host, nil)
       sockaddr = Socket.pack_sockaddr_in(port, addr[0][3])
 
-      socket = Socket.new(Socket.const_get(addr[0][0]), Socket::SOCK_STREAM, 0)
-      socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+      @socket = Socket.new(Socket.const_get(addr[0][0]), Socket::SOCK_STREAM, 0)
+      @socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
 
       begin
         # Initiate the socket connection in the background. If it doesn't fail 
         # immediately it will raise an IO::WaitWritable (Errno::EINPROGRESS) 
         # indicating the connection is in progress.
-        socket.connect_nonblock(sockaddr)
+        @socket.connect_nonblock(sockaddr)
       rescue IO::WaitWritable
         # IO.select will block until the socket is writable or the timeout
         # is exceeded, whichever comes first.
-        unless IO.select(nil, [socket], nil, timeout)
+        unless IO.select(nil, [@socket], nil, timeout)
           # IO.select returns nil when the socket is not ready before timeout 
           # seconds have elapsed
-          socket.close
+          @socket.close
           raise Errno::ETIMEDOUT
         end
 
         begin
           # Verify there is now a good connection.
-          socket.connect_nonblock(sockaddr)
+          @socket.connect_nonblock(sockaddr)
         rescue Errno::EISCONN
           # The socket is connected, we're good!
         end
       end
+    end
 
-      socket
+    def read(num_bytes, timeout: nil)
+      unless IO.select([@socket], nil, nil, timeout)
+        raise Errno::ETIMEDOUT
+      end
+
+      @socket.read(num_bytes)
+    end
+
+    def write(bytes, timeout: nil)
+      unless IO.select(nil, [@socket], nil, timeout)
+        raise Errno::ETIMEDOUT
+      end
+
+      @socket.write(bytes)
+    end
+
+    def close
+      @socket.close
+    end
+
+    def set_encoding(encoding)
+      @socket.set_encoding(encoding)
     end
   end
 end
--- spec/socket_with_timeout_spec.rb
+++ spec/socket_with_timeout_spec.rb
@@ -9,11 +9,34 @@ describe Kafka::SocketWithTimeout, ".open" do
     start = Time.now
 
     expect {
-      Kafka::SocketWithTimeout.open(host, port, timeout: timeout)
+      Kafka::SocketWithTimeout.new(host, port, timeout: timeout)
     }.to raise_exception(Errno::ETIMEDOUT)
 
     finish = Time.now
 
     expect(finish - start).to be < allowed_time
+  end
+
+  describe "#read" do
+    it "times out after the specified amount of time" do
+      host = "localhost"
+      server = TCPServer.new(host, 0)
+      port = server.addr[1]
+
+      timeout = 0.1
+      allowed_time = timeout + 0.1
+
+      socket = Kafka::SocketWithTimeout.new(host, port, timeout: 1)
+
+      start = Time.now
+
+      expect {
+        socket.read(4, timeout: timeout)
+      }.to raise_exception(Errno::ETIMEDOUT)
+
+      finish = Time.now
+
+      expect(finish - start).to be < allowed_time
+    end
   end
 end
