d31da1e1fac8774ad844dfbe5d3e94be78e6e163
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-02-04 14:44:49 +0100

    Add Kafka::Instrumentation

---
+++ lib/kafka/instrumentation.rb
@@ -0,0 +1,13 @@
+module Kafka
+  class NullInstrumentation
+    def self.instrument(name, payload = {})
+      yield payload if block_given?
+    end
+  end
+
+  if defined?(ActiveSupport::Notifications)
+    Instrumentation = ActiveSupport::Notifications 
+  else
+    Instrumentation = NullInstrumentation
+  end
+end
