802d4b8d6b76fba89486f58c6ac11af97874b6f8
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-02-19 14:09:46 +0100

    Add support for Gzip compression

--- lib/kafka/compression.rb
+++ lib/kafka/compression.rb
@@ -1,16 +1,20 @@
 require "kafka/snappy_codec"
+require "kafka/gzip_codec"
 
 module Kafka
   module Compression
     def self.find_codec(name)
       case name
+      when nil then nil
       when :snappy then SnappyCodec.new
-      else nil
+      when :gzip then GzipCodec.new
+      else raise "Unknown compression codec #{name}"
       end
     end
 
     def self.find_codec_by_id(codec_id)
       case codec_id
+      when 1 then GzipCodec.new
       when 2 then SnappyCodec.new
       else raise "Unknown codec id #{codec_id}"
       end
--- spec/functional/compression_spec.rb
+++ spec/functional/compression_spec.rb
@@ -3,23 +3,35 @@ require "snappy"
 describe "Compression", functional: true do
   let(:logger) { Logger.new(LOG) }
   let(:kafka) { Kafka.new(seed_brokers: KAFKA_BROKERS, client_id: "test", logger: logger) }
-  let(:producer) do
-    kafka.get_producer(
-      compression_codec: :snappy,
-      max_retries: 0,
-      retry_backoff: 0
-    )
-  end
 
   before do
     require "test_cluster"
   end
 
-  after do
-    producer.shutdown
+  example "producing and consuming snappy-compressed messages" do
+    producer = kafka.get_producer(
+      compression_codec: :snappy,
+      max_retries: 0,
+      retry_backoff: 0
+    )
+
+    producer.produce("message1", topic: "test-messages", partition: 0)
+    producer.produce("message2", topic: "test-messages", partition: 0)
+
+    producer.deliver_messages
+
+    messages = kafka.fetch_messages(topic: "test-messages", partition: 0, offset: 0)
+
+    expect(messages.last(2).map(&:value)).to eq ["message1", "message2"]
   end
 
-  example "producing and consuming compressed messages" do
+  example "producing and consuming gzip-compressed messages" do
+    producer = kafka.get_producer(
+      compression_codec: :gzip,
+      max_retries: 0,
+      retry_backoff: 0
+    )
+
     producer.produce("message1", topic: "test-messages", partition: 0)
     producer.produce("message2", topic: "test-messages", partition: 0)
 
