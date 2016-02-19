605cf6e2185738719064b19d6da8d8e025d12969
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-02-19 15:05:46 +0100

    Add an asynchronous producer
    This producer does all the IO work in the background and offers
    automatic delivery policies.

---
+++ spec/async_producer_spec.rb
@@ -0,0 +1,55 @@
+class FakeSyncProducer
+  def initialize(mutex)
+    @mutex = mutex
+  end
+
+  def produce(*args)
+    @mutex.lock
+  end
+
+  def deliver_messages
+  end
+
+  def shutdown
+  end
+end
+
+describe Kafka::AsyncProducer do
+  describe "#produce" do
+    it "raises BufferOverflow if the queue exceeds the defined max size" do
+      # The sync producer will be blocked trying to grab the mutex.
+      mutex = Mutex.new
+      mutex.lock
+
+      sync_producer = FakeSyncProducer.new(mutex)
+
+      producer = Kafka::AsyncProducer.new(
+        sync_producer: sync_producer,
+        max_queue_size: 2,
+      )
+
+      expect {
+        3.times do
+          producer.produce("hello", topic: "greetings")
+        end
+      }.to raise_exception(Kafka::BufferOverflow)
+    end
+  end
+
+  describe "#deliver_messages" do
+    it "handles when the sync producer fails to deliver messages" do
+      sync_producer = double(:sync_producer, shutdown: nil, produce: nil)
+
+      producer = Kafka::AsyncProducer.new(
+        sync_producer: sync_producer,
+      )
+
+      producer.produce("hello", topic: "greetings")
+
+      allow(sync_producer).to receive(:deliver_messages).and_raise(Kafka::FailedToSendMessages)
+
+      producer.deliver_messages
+      producer.shutdown
+    end
+  end
+end
---
+++ spec/functional/async_producer_spec.rb
@@ -0,0 +1,75 @@
+describe "Producer API", functional: true do
+  let(:logger) { Logger.new(LOG) }
+  let(:kafka) { Kafka.new(seed_brokers: KAFKA_BROKERS, client_id: "test", logger: logger) }
+  let(:producer) { kafka.async_producer(max_retries: 1, retry_backoff: 0) }
+
+  before do
+    require "test_cluster"
+  end
+
+  after do
+    producer.shutdown
+  end
+
+  example "writing messages using async producer" do
+    value1 = rand(10_000).to_s
+    value2 = rand(10_000).to_s
+
+    producer.produce(value1, key: "x", topic: "test-messages", partition: 0)
+    producer.produce(value2, key: "y", topic: "test-messages", partition: 1)
+
+    producer.deliver_messages
+
+    # Wait for everything to be delivered.
+    producer.shutdown
+
+    message1 = kafka.fetch_messages(topic: "test-messages", partition: 0, offset: :earliest).last
+    message2 = kafka.fetch_messages(topic: "test-messages", partition: 1, offset: :earliest).last
+
+    expect(message1.value).to eq value1
+    expect(message2.value).to eq value2
+  end
+
+  example "automatically delivering messages with a fixed time interval" do
+    producer = kafka.async_producer(delivery_interval: 0.1)
+
+    value = rand(10_000).to_s
+    producer.produce(value, topic: "test-messages", partition: 0)
+
+    sleep 0.2
+
+    messages = kafka.fetch_messages(
+      topic: "test-messages",
+      partition: 0,
+      offset: 0,
+      max_wait_time: 0.1,
+    )
+
+    expect(messages.last.value).to eq value
+
+    producer.shutdown
+  end
+
+  example "automatically delivering messages when a buffer threshold is reached" do
+    producer = kafka.async_producer(delivery_threshold: 5)
+
+    values = 5.times.map { rand(10_000).to_s }
+
+    values.each do |value|
+      producer.produce(value, topic: "test-messages", partition: 0)
+    end
+
+    sleep 0.2
+
+    messages = kafka.fetch_messages(
+      topic: "test-messages",
+      partition: 0,
+      offset: 0,
+      max_wait_time: 0,
+    )
+
+    expect(messages.last(5).map(&:value)).to eq values
+
+    producer.shutdown
+  end
+end
