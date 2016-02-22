83f74544c6707bd10038013a860b20eacdbff20e
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-02-22 13:04:03 +0100

    Make the compression spec more resilient

--- spec/functional/compression_spec.rb
+++ spec/functional/compression_spec.rb
@@ -15,12 +15,18 @@ describe "Compression", functional: true do
       retry_backoff: 0
     )
 
+    last_offset = fetch_last_offset
+
     producer.produce("message1", topic: "test-messages", partition: 0)
     producer.produce("message2", topic: "test-messages", partition: 0)
 
     producer.deliver_messages
 
-    messages = kafka.fetch_messages(topic: "test-messages", partition: 0, offset: 0)
+    messages = kafka.fetch_messages(
+      topic: "test-messages",
+      partition: 0,
+      offset: last_offset + 1,
+    )
 
     expect(messages.last(2).map(&:value)).to eq ["message1", "message2"]
   end
@@ -32,13 +38,24 @@ describe "Compression", functional: true do
       retry_backoff: 0
     )
 
+    last_offset = fetch_last_offset
+
     producer.produce("message1", topic: "test-messages", partition: 0)
     producer.produce("message2", topic: "test-messages", partition: 0)
 
     producer.deliver_messages
 
-    messages = kafka.fetch_messages(topic: "test-messages", partition: 0, offset: 0)
+    messages = kafka.fetch_messages(
+      topic: "test-messages",
+      partition: 0,
+      offset: last_offset + 1,
+    )
 
     expect(messages.last(2).map(&:value)).to eq ["message1", "message2"]
+  end
+
+  def fetch_last_offset
+    last_message = kafka.fetch_messages(topic: "test-messages", partition: 0, offset: 0).last
+    last_message ? last_message.offset : 0
   end
 end
