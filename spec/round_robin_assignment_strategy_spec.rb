b76474cd51a3d3f74956dc8af3d069c1eb2be58b
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-03-02 14:54:10 +0100

    Add a spec for RoundRobinAssignmentStrategy

---
+++ spec/round_robin_assignment_strategy_spec.rb
@@ -0,0 +1,24 @@
+describe Kafka::RoundRobinAssignmentStrategy do
+  it "assigns all partitions" do
+    cluster = double(:cluster)
+    strategy = described_class.new(cluster: cluster)
+
+    members = (0...10).map {|i| "member#{i}" }
+    topics = ["greetings"]
+    partitions = (0...30).map {|i| double(:"partition#{i}", partition_id: i) }
+
+    allow(cluster).to receive(:partitions_for) { partitions }
+
+    assignments = strategy.assign(members: members, topics: topics)
+
+    partitions.each do |partition|
+      member = assignments.values.find {|assignment|
+        assignment.topics.find {|topic, partitions|
+          partitions.include?(partition.partition_id)
+        }
+      }
+
+      expect(member).to_not be_nil
+    end
+  end
+end
