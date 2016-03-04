ec87abe6e862fc031a4690780b5932f3ece7a310
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-03-04 14:13:12 +0100

    Allow specifying the offset commit policy as a threshold
    The threshold represents the number of messages that can be processed
    before an offset commit is required, thus putting an upper bound on
    duplicate message processing in case a consumer dies.

--- lib/kafka/offset_manager.rb
+++ lib/kafka/offset_manager.rb
@@ -1,10 +1,12 @@
 module Kafka
   class OffsetManager
-    def initialize(group:, logger:, commit_interval: 10)
+    def initialize(group:, logger:, commit_interval:, commit_threshold:)
       @group = group
       @logger = logger
       @commit_interval = commit_interval
+      @commit_threshold = commit_threshold
 
+      @uncommitted_offsets = 0
       @processed_offsets = {}
       @default_offsets = {}
       @committed_offsets = nil
@@ -16,6 +18,7 @@ module Kafka
     end
 
     def mark_as_processed(topic, partition, offset)
+      @uncommitted_offsets += 1
       @processed_offsets[topic] ||= {}
       @processed_offsets[topic][partition] = offset + 1
     end
@@ -32,19 +35,24 @@ module Kafka
 
     def commit_offsets
       unless @processed_offsets.empty?
-        @logger.info "Committing offsets"
+        @logger.info "Committing offsets for #{@uncommitted_offsets} messages"
+
         @group.commit_offsets(@processed_offsets)
+
         @last_commit = Time.now
+        @processed_offsets.clear
+        @uncommitted_offsets = 0
       end
     end
 
     def commit_offsets_if_necessary
-      if seconds_since_last_commit >= @commit_interval
+      if seconds_since_last_commit >= @commit_interval || commit_threshold_reached?
         commit_offsets
       end
     end
 
     def clear_offsets
+      @uncommitted_offsets = 0
       @processed_offsets.clear
       @committed_offsets = nil
     end
@@ -58,6 +66,10 @@ module Kafka
     def committed_offset_for(topic, partition)
       @committed_offsets ||= @group.fetch_offsets
       @committed_offsets.offset_for(topic, partition)
+    end
+
+    def commit_threshold_reached?
+      @commit_threshold != 0 && @uncommitted_offsets >= @commit_threshold
     end
   end
 end
