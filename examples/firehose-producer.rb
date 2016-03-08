caa59765993e4c20cdf1099629bc93fe4fa35a20
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-03-08 14:08:02 +0100

    Load the env from .env

--- examples/firehose-producer.rb
+++ examples/firehose-producer.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
 
 require "kafka"
+require "dotenv"
+
+Dotenv.load
 
 KAFKA_CLIENT_CERT = ENV.fetch("KAFKA_CLIENT_CERT")
 KAFKA_CLIENT_CERT_KEY = ENV.fetch("KAFKA_CLIENT_CERT_KEY")
