c02bb85c918622a2731e1538b3de43da9c2632bf
Masahiro Nakagawa <repeatedly@gmail.com>
2016-06-10 04:52:03 +0900

    Use struct instead of class to improve performance

--- lib/kafka/producer.rb
+++ lib/kafka/producer.rb
@@ -185,12 +185,13 @@ module Kafka
       create_time = Time.now
 
       message = PendingMessage.new(
-        value: value,
-        key: key,
-        topic: topic,
-        partition: partition,
-        partition_key: partition_key,
-        create_time: create_time,
+        value,
+        key,
+        topic,
+        partition,
+        partition_key,
+        create_time,
+        key.to_s.bytesize + value.to_s.bytesize
       )
 
       if buffer_size >= @max_buffer_size
