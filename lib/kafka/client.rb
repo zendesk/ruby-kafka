fde0e98a5def5383d4369fe8592bb36d337e63ff
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-06-15 09:52:33 -0700

    Fix incorrect use of PendingMessage

--- lib/kafka/client.rb
+++ lib/kafka/client.rb
@@ -63,12 +63,13 @@ module Kafka
       create_time = Time.now
 
       message = PendingMessage.new(
-        value: value,
-        key: key,
-        topic: topic,
-        partition: partition,
-        partition_key: partition_key,
-        create_time: create_time,
+        value,
+        key,
+        topic,
+        partition,
+        partition_key,
+        create_time,
+        key.to_s.bytesize + value.to_s.bytesize
       )
 
       if partition.nil?
