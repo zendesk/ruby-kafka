d54d27aa5b74b52b494f78f92aca480369928423
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-08-01 11:57:47 +0200

    Simplify functional spec

--- spec/functional/consumer_group_spec.rb
+++ spec/functional/consumer_group_spec.rb
@@ -60,31 +60,29 @@ describe "Consumer API", functional: true do
   example "consuming messages from the end of a topic" do
     sent_messages = 1_000
 
+    num_partitions = 1
+    topic = create_random_topic(num_partitions: num_partitions)
     group_id = "test#{rand(1000)}"
 
-    threads = 2.times.map do |thread_id|
-      t = Thread.new do
-        received_messages = 0
+    consumer_thread = Thread.new do
+      received_messages = 0
 
-        kafka = Kafka.new(seed_brokers: kafka_brokers, client_id: "test", logger: logger)
-        consumer = kafka.consumer(group_id: group_id)
-        consumer.subscribe(topic, start_from_beginning: false)
+      kafka = Kafka.new(seed_brokers: kafka_brokers, client_id: "test", logger: logger)
+      consumer = kafka.consumer(group_id: group_id)
+      consumer.subscribe(topic, start_from_beginning: false)
 
-        consumer.each_message do |message|
-          if message.value.nil?
-            consumer.stop
-          else
-            received_messages += 1
-          end
+      consumer.each_message do |message|
+        if message.value.nil?
+          consumer.stop
+        else
+          received_messages += 1
         end
-
-        received_messages
       end
 
-      t.abort_on_exception = true
-
-      t
+      received_messages
     end
+
+    consumer_thread.abort_on_exception = true
 
     sleep 30
 
@@ -109,7 +107,7 @@ describe "Consumer API", functional: true do
       producer.deliver_messages
     end
 
-    received_messages = threads.map(&:value).inject(0, &:+)
+    received_messages = consumer_thread.value
 
     expect(received_messages).to eq sent_messages
   end
