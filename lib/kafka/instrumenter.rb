fbb917f933c07dc2183f771d9adcb8ac2f684aad
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-12-05 16:49:20 +0100

    Fetch messages concurrently from all brokers
    The consumer will write a request to each broker it needs messages from
    before reading any responses.

--- lib/kafka/instrumenter.rb
+++ lib/kafka/instrumenter.rb
@@ -6,19 +6,35 @@ module Kafka
       @default_payload = default_payload
 
       if defined?(ActiveSupport::Notifications)
-        @backend = ActiveSupport::Notifications
+        @backend = ActiveSupport::Notifications.instrumenter
       else
         @backend = nil
       end
     end
 
-    def instrument(event_name, payload = {}, &block)
+    def instrument(event_name, payload = {})
       if @backend
         payload.update(@default_payload)
 
-        @backend.instrument("#{event_name}.#{NAMESPACE}", payload, &block)
+        @backend.instrument("#{event_name}.#{NAMESPACE}", payload) { yield payload if block_given? }
       else
-        block.call(payload) if block
+        yield payload if block_given?
+      end
+    end
+
+    def start(event_name, payload = {})
+      if @backend
+        payload.update(@default_payload)
+
+        @backend.start("#{event_name}.#{NAMESPACE}", payload)
+      end
+    end
+
+    def finish(event_name, payload = {})
+      if @backend
+        payload.update(@default_payload)
+
+        @backend.finish("#{event_name}.#{NAMESPACE}", payload)
       end
     end
   end
@@ -31,6 +47,14 @@ module Kafka
 
     def instrument(event_name, payload = {}, &block)
       @backend.instrument(event_name, @extra_payload.merge(payload), &block)
+    end
+
+    def start(event_name, payload = {})
+      @backend.start(event_name, @extra_payload.merge(payload))
+    end
+
+    def finish(event_name, payload = {})
+      @backend.finish(event_name, @extra_payload.merge(payload))
     end
   end
 end
