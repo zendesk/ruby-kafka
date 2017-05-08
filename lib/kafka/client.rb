59c63554cf545478d655b4a9a9bbb27fd52c3d4b
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2017-05-08 10:46:10 +0200

    Small cleanups

--- lib/kafka/client.rb
+++ lib/kafka/client.rb
@@ -417,21 +417,21 @@ module Kafka
 
     # Retrieve the offset of the last message in each partition of the specified topics.
     #
-    # @param topics [Array<String>] topic names
+    # @param topics [Array<String>] topic names.
     # @return [Hash]
     # @example
-    #   last_offsets_for('topic-1', 'topic-2')  ->
-    #   {
-    #     'topic-1' => { 0 => 100, 1 => 100 },
-    #     'topic-2' => { 0 => 100, 1 => 100 }
-    #   }
+    #   last_offsets_for('topic-1', 'topic-2') # =>
+    #   # {
+    #   #   'topic-1' => { 0 => 100, 1 => 100 },
+    #   #   'topic-2' => { 0 => 100, 1 => 100 }
+    #   # }
     def last_offsets_for(*topics)
       @cluster.add_target_topics(topics)
-      topics.map do |topic|
-        partition_ids     = @cluster.partitions_for(topic).collect(&:partition_id)
+      topics.map {|topic|
+        partition_ids = @cluster.partitions_for(topic).collect(&:partition_id)
         partition_offsets = @cluster.resolve_offsets(topic, partition_ids, :latest)
-        [topic, partition_offsets.collect { |k, v| [k, v - 1] } .to_h]
-      end.to_h
+        [topic, partition_offsets.collect { |k, v| [k, v - 1] }.to_h]
+      }.to_h
     end
 
     # Closes all connections to the Kafka brokers and frees up used resources.
