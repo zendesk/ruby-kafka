f388b87167df2861f254fcdf4b93044250b88110
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2017-10-20 12:48:18 +0200

    Add simple consumer group example

---
+++ examples/consumer-group.rb
@@ -0,0 +1,26 @@
+$LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
+
+require "kafka"
+
+logger = Logger.new(STDOUT)
+brokers = ENV.fetch("KAFKA_BROKERS", "localhost:9092").split(",")
+
+# Make sure to create this topic in your Kafka cluster or configure the
+# cluster to auto-create topics.
+topic = "text"
+
+kafka = Kafka.new(
+  seed_brokers: brokers,
+  client_id: "test",
+  socket_timeout: 20,
+  logger: logger,
+)
+
+consumer = kafka.consumer(group_id: "test")
+consumer.subscribe(topic)
+
+trap("TERM") { consumer.stop }
+
+consumer.each_message do |message|
+  puts message.value
+end
