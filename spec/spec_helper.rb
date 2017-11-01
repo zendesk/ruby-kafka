82c06959fc3267de961f51ddeafafc06f06c3a9b
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2017-11-01 12:50:53 +0100

    Fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,17 @@ require "dotenv"
 require "logger"
 require "rspec-benchmark"
 require "colored"
+require "securerandom"
 
 Dotenv.load
 
 LOGGER = Logger.new(ENV.key?("LOG_TO_STDERR") ? $stderr : "test-#{Time.now.to_i}.log")
 LOGGER.level = Logger.const_get(ENV.fetch("LOG_LEVEL", "INFO"))
+
+KAFKA_BROKERS = ENV.fetch("KAFKA_BROKERS", "localhost:9092").split(",")
+
+# A unique id for the test run, used to namespace global resources.
+RUN_ID = SecureRandom.hex(8)
 
 class LogFormatter < Logger::Formatter
   def call(severity, time, progname, msg)
@@ -44,7 +50,7 @@ module SpecHelpers
     @@topic_number ||= 0
     @@topic_number += 1
 
-    "topic-#{@@topic_number}"
+    "#{RUN_ID}-topic-#{@@topic_number}"
   end
 
   def create_random_topic(*args)
@@ -62,7 +68,7 @@ module FunctionalSpecHelpers
   def self.included(base)
     base.class_eval do
       let(:logger) { LOGGER }
-      let(:kafka_brokers) { ["localhost:9092", "localhost:9093", "localhost:9094"] }
+      let(:kafka_brokers) { KAFKA_BROKERS }
       let(:kafka) { Kafka.new(seed_brokers: kafka_brokers, client_id: "test", logger: logger) }
 
       after { kafka.close rescue nil }
