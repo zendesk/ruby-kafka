1a3112c426619767d45f2e1851dec109e69a4280
Jon Börjesson <jon@spuun.se>
2017-11-02 07:48:24 +0100

    Added specs for scram

--- spec/fake_server.rb
+++ spec/fake_server.rb
@@ -1,5 +1,5 @@
 class FakeServer
-  SUPPORTED_MECHANISMS = ['PLAIN']
+  SUPPORTED_MECHANISMS = ['PLAIN','SCRAM-SHA-256', 'SCRAM-SHA-512']
 
   def self.start(server)
     thread = Thread.new { new(server).start }
@@ -29,49 +29,136 @@ class FakeServer
   end
 
   def handle_client(client)
+    encoder = Kafka::Protocol::Encoder.new(client)
+    decoder = Kafka::Protocol::Decoder.new(client)
+
     loop do
-      request_bytes = Kafka::Protocol::Decoder.new(client).bytes
-      encoder = Kafka::Protocol::Encoder.new(client)
+      request_bytes = decoder.bytes
+      request_data = Kafka::Protocol::Decoder.new(StringIO.new(request_bytes));
+      api_key = request_data.int16
+      api_version = request_data.int16
+      correlation_id = request_data.int32
+      client_id = request_data.string
 
-      # Special case for authentication
-      if @authenticating
-        case @auth_mechanism
-        when 'PLAIN'
-          _authzid, username, password = request_bytes.split("\000")
-
-          if username == 'spec_username' && password == 'spec_password'
-            # Successfully Authenticated, send back empty string
-            encoder.write_bytes('')
-            @authenticating = false
-          end
-        else
-          # Unknown mechanism
-        end
-
-        break
-      end
-
-      request_decoder = Kafka::Protocol::Decoder.new(StringIO.new(request_bytes))
-      api_key = request_decoder.int16
-      api_version = request_decoder.int16
-      correlation_id = request_decoder.int32
-      client_id = request_decoder.string
-
-      message = request_decoder.string
+      message = request_data.string
 
       response = StringIO.new
       response_encoder = Kafka::Protocol::Encoder.new(response)
       response_encoder.write_int32(correlation_id)
 
-      if api_key == 17 # SASL Authentication
-        response_encoder.write_int16(0) # no errors
-        response_encoder.write_array(SUPPORTED_MECHANISMS) { |msg| response_encoder.write_string(msg) }
-        @authenticating = true
-        @auth_mechanism = message
+      case api_key
+      when 17 then
+        response_encoder.write_int16(0)
+        response_encoder.write_array(SUPPORTED_MECHANISMS) { |m| response_encoder.write_string(m) }
+        encoder.write_bytes(response.string)
+        auth(message, encoder, decoder)
+        break
       else
         response_encoder.write_string(message)
+        encoder.write_bytes(response.string)
       end
-      encoder.write_bytes(response.string)
     end
   end
+
+  def auth(auth_mechanism, encoder, decoder)
+    case auth_mechanism
+    when 'PLAIN'
+      message = decoder.bytes
+      _authzid, username, password = message.split("\000")
+      if username == 'spec_username' && password == 'spec_password'
+        encoder.write_bytes('')
+      end
+    when 'SCRAM-SHA-256', 'SCRAM-SHA-512'
+      scram_sasl_authenticate(auth_mechanism[6..-1],encoder, decoder)
+    else
+      puts "UNKNOWN AUTH MECHANISM"
+    end
+  end
+
+  def scram_sasl_authenticate (algorithm, encoder, decoder)
+    zk_username = 'spec_username'
+    zk_data = {
+      'SHA-512': {
+        salt: 'ODVhbzNqcGdneDR5ZzIzbmJpcnpodmdxcg==',
+        stored_key: 'kfUpWelvXn406F1rKx3gE9Nz6qBBI+7v1Dg2n8QSNy9ZA1vU1jxYKOMRVV9188TDxhQe6Te0D8R2t0r5YFILnA==',
+        server_key: 'CDkccMty/z9z7KUciVixhIuPLV53QtMHT2SbJUbvNqdaqGvtkTwDgMCLjWKqMKkUvnInYziJh/YfRKYNoLEnaQ==',
+        iterations: 4096
+      },
+      'SHA-256': {
+        salt: 'MWVkNGdvam9qNG4yYmt1dG82ZGxrY3ppM3c=',
+        stored_key: 'W28WpOjPl87SPMfFZsuyA5Yor0Z/q4+VZJlZqzDfgsI=',
+        server_key: '17y/jubvVV8cWGxhaMN/8eOFTvnaYQ9f/JJmNszmOFI=',
+        iterations: 4096
+      }
+    }
+    @scram_mechanism = algorithm
+    algorithm = algorithm.to_sym
+    request_bytes = decoder.bytes
+    _, _, userdata, nouncedata =  request_bytes.split(',')
+    _, username = userdata.split('=')
+    _, client_nounce = nouncedata.split('=')
+
+    return if username != zk_username
+
+    client_first_message_bare = "#{userdata},#{nouncedata}"
+    server_nounce = SecureRandom.urlsafe_base64(8)
+
+    salt64 = zk_data[algorithm][:salt]
+    iterations = zk_data[algorithm][:iterations]
+    stored_key = Base64.strict_decode64(zk_data[algorithm][:stored_key])
+    server_key = Base64.strict_decode64(zk_data[algorithm][:server_key])
+    salt = Base64.strict_decode64(salt64)
+
+    server_first_message = "r=#{client_nounce}#{server_nounce},s=#{salt64},i=#{iterations}"
+    encoder.write_bytes(server_first_message)
+
+    request_bytes = decoder.bytes
+    c, r, proofdata = request_bytes.split(",")
+    _, proof = proofdata.split("=",2)
+
+    client_last_message_without_proof = "#{c},#{r}"
+    auth_message = [client_first_message_bare, server_first_message, client_last_message_without_proof].join(',')
+    salted_password = hi('spec_password', salt, iterations)
+    client_key = hmac(salted_password, 'Client Key')
+    client_signature = hmac(stored_key, auth_message)
+    server_proof = Base64.strict_encode64(xor(client_key, client_signature))
+
+    return if server_proof != proof
+
+    server_signature = Base64.strict_encode64(hmac(server_key, auth_message))
+    encoder.write_bytes("v=#{server_signature}")
+  end
+
+  def digest
+    @digest ||= case @scram_mechanism
+                when Kafka::SCRAM_SHA256
+                  OpenSSL::Digest::SHA256.new.freeze
+                when Kafka::SCRAM_SHA512
+                  OpenSSL::Digest::SHA512.new.freeze
+                else
+                  raise StandardError, "Unknown mechanism '#{@scram_mechanism}'"
+                end
+  end
+
+  def xor(first, second)
+    first.bytes.zip(second.bytes).map{ |(a,b)| (a ^ b).chr }.join('')
+  end
+
+  def hi(str, salt, iterations)
+    OpenSSL::PKCS5.pbkdf2_hmac(
+      str,
+      salt,
+      iterations,
+      digest.size,
+      digest)
+  end
+
+  def hmac(data, key)
+    OpenSSL::HMAC.digest(digest, data, key)
+  end
+
+  def h(str)
+    digest.digest(str)
+  end
+
 end
---
+++ spec/sasl_scram_authenticator_spec.rb
@@ -0,0 +1,86 @@
+require 'fake_server'
+require 'kafka/sasl_scram_authenticator'
+
+describe Kafka::SaslScramAuthenticator do
+  let(:logger) { LOGGER }
+  let(:host) { "127.0.0.1" }
+  let(:server) { TCPServer.new(host, 0) }
+  let(:port) { server.addr[1] }
+  let(:authenticator) {
+    instance_double(Kafka::SaslAuthenticator, authenticate!: true)
+  }
+
+  let(:connection) {
+    Kafka::Connection.new(
+      host: host,
+      port: port,
+      client_id: "test",
+      logger: logger,
+      instrumenter: Kafka::Instrumenter.new(client_id: "test"),
+      connect_timeout: 0.1,
+      socket_timeout: 0.1,
+      authenticator: authenticator
+    )
+  }
+
+  let!(:broker) { FakeServer.start(server) }
+
+  describe '#authenticate!' do
+   context 'when correct username/password using SHA-256' do
+      let(:sasl_scram_authenticator) {
+        Kafka::SaslScramAuthenticator.new(
+          'spec_username',
+          'spec_password',
+          logger: logger,
+          mechanism: Kafka::SCRAM_SHA256
+        )
+      }
+
+      it 'successfully authenticates' do
+        expect(sasl_scram_authenticator.authenticate!(connection)).to be_truthy
+      end
+    end
+    context 'when correct username/password using SHA-512' do
+      let(:sasl_scram_authenticator) {
+        Kafka::SaslScramAuthenticator.new(
+          'spec_username',
+          'spec_password',
+          logger: logger,
+          mechanism: Kafka::SCRAM_SHA512
+        )
+      }
+
+      it 'successfully authenticates' do
+        expect(sasl_scram_authenticator.authenticate!(connection)).to be_truthy
+      end
+    end
+    context 'when incorrect username' do
+      let(:sasl_scram_authenticator) {
+        Kafka::SaslScramAuthenticator.new(
+          'spec_wrong_username',
+          'spec_password',
+          logger: logger,
+          mechanism: Kafka::SCRAM_SHA256
+        )
+      }
+
+      it 'raise error' do
+        expect { sasl_scram_authenticator.authenticate!(connection) }.to raise_error(Kafka::FailedScramAuthentication)
+      end
+    end
+    context 'when incorrect password' do
+      let(:sasl_scram_authenticator) {
+        Kafka::SaslScramAuthenticator.new(
+          'spec_username',
+          'spec_wrong_password',
+          logger: logger,
+          mechanism: Kafka::SCRAM_SHA256
+        )
+      }
+
+      it 'raise error' do
+        expect { sasl_scram_authenticator.authenticate!(connection) }.to raise_error(Kafka::FailedScramAuthentication)
+      end
+    end
+  end
+end
