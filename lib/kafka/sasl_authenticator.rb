e34d7fc4be1aa95508418a01bfc5a3e2f9990186
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2017-11-08 14:02:35 +0100

    Refactor SASL authentication code

--- lib/kafka/sasl_authenticator.rb
+++ lib/kafka/sasl_authenticator.rb
@@ -1,6 +1,6 @@
-require 'kafka/sasl_gssapi_authenticator'
-require 'kafka/sasl_plain_authenticator'
-require 'kafka/sasl_scram_authenticator'
+require 'kafka/sasl/plain'
+require 'kafka/sasl/gssapi'
+require 'kafka/sasl/scram'
 
 module Kafka
   class SaslAuthenticator
@@ -8,73 +8,41 @@ module Kafka
                    sasl_plain_authzid:, sasl_plain_username:, sasl_plain_password:,
                    sasl_scram_username:, sasl_scram_password:, sasl_scram_mechanism:)
       @logger = logger
-      @sasl_gssapi_principal = sasl_gssapi_principal
-      @sasl_gssapi_keytab = sasl_gssapi_keytab
-      @sasl_plain_authzid = sasl_plain_authzid
-      @sasl_plain_username = sasl_plain_username
-      @sasl_plain_password = sasl_plain_password
-      @sasl_scram_username = sasl_scram_username
-      @sasl_scram_password = sasl_scram_password
-      @sasl_scram_mechanism = sasl_scram_mechanism
+
+      @plain = Sasl::Plain.new(
+        authzid: sasl_plain_authzid,
+        username: sasl_plain_username,
+        password: sasl_plain_password,
+        logger: @logger,
+      )
+
+      @gssapi = Sasl::Gssapi.new(
+        principal: sasl_gssapi_principal,
+        keytab: sasl_gssapi_keytab,
+        logger: @logger,
+      )
+
+      @scram = Sasl::Scram.new(
+        username: sasl_scram_username,
+        password: sasl_scram_password,
+        mechanism: sasl_scram_mechanism,
+        logger: @logger,
+      )
     end
 
     def authenticate!(connection)
-      if authenticate_using_sasl_gssapi?
-        sasl_gssapi_authenticate(connection)
-      elsif authenticate_using_sasl_plain?
-        sasl_plain_authenticate(connection)
-      elsif authenticate_using_sasl_scram?
-        sasl_scram_authenticate(connection)
+      mechanism = [@gssapi, @plain, @scram].find(&:configured?)
+
+      return if mechanism.nil?
+
+      ident = mechanism.ident
+      response = connection.send_request(Kafka::Protocol::SaslHandshakeRequest.new(ident))
+
+      unless response.error_code == 0 && response.enabled_mechanisms.include?(ident)
+        raise Kafka::Error, "#{ident} is not supported."
       end
-    end
 
-    private
-
-    def sasl_scram_authenticate(connection)
-      auth = SaslScramAuthenticator.new(
-        username: @sasl_scram_username,
-        password: @sasl_scram_password,
-        logger: @logger,
-        mechanism: @sasl_scram_mechanism,
-        connection: connection
-      )
-
-      auth.authenticate!
-    end
-
-    def sasl_gssapi_authenticate(connection)
-      auth = SaslGssapiAuthenticator.new(
-        logger: @logger,
-        sasl_gssapi_principal: @sasl_gssapi_principal,
-        sasl_gssapi_keytab: @sasl_gssapi_keytab,
-        connection: connection
-      )
-
-      auth.authenticate!
-    end
-
-    def sasl_plain_authenticate(connection)
-      auth = SaslPlainAuthenticator.new(
-        logger: @logger,
-        authzid: @sasl_plain_authzid,
-        username: @sasl_plain_username,
-        password: @sasl_plain_password,
-        connection: connection
-      )
-
-      auth.authenticate!
-    end
-
-    def authenticate_using_sasl_scram?
-      @sasl_scram_username && @sasl_scram_password
-    end
-
-    def authenticate_using_sasl_gssapi?
-      !@ssl_context && @sasl_gssapi_principal && !@sasl_gssapi_principal.empty?
-    end
-
-    def authenticate_using_sasl_plain?
-      @sasl_plain_authzid && @sasl_plain_username && @sasl_plain_password
+      mechanism.authenticate!(connection.to_s, connection.encoder, connection.decoder)
     end
   end
 end
