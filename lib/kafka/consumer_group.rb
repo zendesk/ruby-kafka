98d64fc9d556710c70c27c10e48d94641fb8e08a
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2017-12-11 00:44:41 +0700

    Integrate group protocol manager into consumer group

--- lib/kafka/consumer_group.rb
+++ lib/kafka/consumer_group.rb
@@ -1,11 +1,11 @@
 require "set"
-require "kafka/round_robin_assignment_strategy"
+require "kafka/group_protocol_manager"
 
 module Kafka
   class ConsumerGroup
     attr_reader :assigned_partitions, :generation_id
 
-    def initialize(cluster:, logger:, group_id:, session_timeout:, retention_time:, instrumenter:)
+    def initialize(cluster:, logger:, group_id:, group_protocols: [], session_timeout:, retention_time:, instrumenter:)
       @cluster = cluster
       @logger = logger
       @group_id = group_id
@@ -17,7 +17,10 @@ module Kafka
       @members = {}
       @topics = Set.new
       @assigned_partitions = {}
-      @assignment_strategy = RoundRobinAssignmentStrategy.new(cluster: @cluster)
+      @group_protocol_manager = GroupProtocolManager.new(
+        cluster: @cluster,
+        group_protocols: group_protocols
+      )
       @retention_time = retention_time
     end
 
@@ -117,6 +120,7 @@ module Kafka
       @instrumenter.instrument("join_group.consumer", group_id: @group_id) do
         response = coordinator.join_group(
           group_id: @group_id,
+          group_protocols: @group_protocol_manager.group_protocols,
           session_timeout: @session_timeout,
           member_id: @member_id,
         )
@@ -132,6 +136,9 @@ module Kafka
 
       @logger.info "Joined group `#{@group_id}` with member id `#{@member_id}`"
       @logger.info "Selected group protocol: `#{@group_protocol}`"
+    rescue InconsistentGroupProtocolError
+      @logger.error "Group `#{@group_id}` doesn't support any of your group protocol. If you are changing the group protocol, please keep old protocol to ensure backward-compatibility until all members are all updated."
+      raise
     rescue UnknownMemberId
       @logger.error "Failed to join group; resetting member id and retrying in 1s..."
 
@@ -151,9 +158,10 @@ module Kafka
       if group_leader?
         @logger.info "Chosen as leader of group `#{@group_id}`"
 
-        group_assignment = @assignment_strategy.assign(
-          members: @members.keys,
-          topics: @topics,
+        group_assignment = @group_protocol_manager.assign(
+          group_protocol: @group_protocol,
+          members: @members,
+          topics: @topics
         )
       end
 
