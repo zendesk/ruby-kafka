899c7c9f3e72ce0e5616d8962c8e7fb91203bc6d
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-01-08 16:06:30 +0100

    Optimize fetched messages
    Avoid copying data from Message to FetchedMessage.

--- lib/kafka/fetched_message.rb
+++ lib/kafka/fetched_message.rb
@@ -1,31 +1,35 @@
 module Kafka
   class FetchedMessage
-
-    # @return [String] the value of the message.
-    attr_reader :value
-
-    # @return [String] the key of the message.
-    attr_reader :key
-
     # @return [String] the name of the topic that the message was written to.
     attr_reader :topic
 
     # @return [Integer] the partition number that the message was written to.
     attr_reader :partition
 
-    # @return [Integer] the offset of the message in the partition.
-    attr_reader :offset
-
-    # @return [Time] the timestamp of the message.
-    attr_reader :create_time
-
-    def initialize(value: nil, key: nil, topic:, partition:, offset:, create_time: nil)
-      @value = value
-      @key = key
+    def initialize(message:, topic:, partition:)
+      @message = message
       @topic = topic
       @partition = partition
-      @offset = offset
-      @create_time = create_time
+    end
+
+    # @return [String] the value of the message.
+    def value
+      @message.value
+    end
+
+    # @return [String] the key of the message.
+    def key
+      @message.key
+    end
+
+    # @return [Integer] the offset of the message in the partition.
+    def offset
+      @message.offset
+    end
+
+    # @return [Time] the timestamp of the message.
+    def create_time
+      @message.create_time
     end
   end
 end
