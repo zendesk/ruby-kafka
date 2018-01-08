ba33f622d591eedb13b4bf9c3bce93fa276dfb22
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-01-08 15:37:36 +0100

    Add a simple benchmark

---
+++ benchmarks/message_encoding.rb
@@ -0,0 +1,21 @@
+require "kafka"
+
+ready "message serialization" do
+  before do
+    message = Kafka::Protocol::Message.new(
+      value: "hello",
+      key: "world",
+    )
+
+    @io = StringIO.new
+    encoder = Kafka::Protocol::Encoder.new(@io)
+    message.encode(encoder)
+
+    @decoder = Kafka::Protocol::Decoder.new(@io)
+  end
+
+  go "decoding" do
+    @io.rewind
+    Kafka::Protocol::Message.decode(@decoder)
+  end
+end
