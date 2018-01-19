650f755170de842945e232414835dfdb9c621bd2
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-01-19 23:13:12 +0700

    Write function tests

--- spec/functional/topic_management_spec.rb
+++ spec/functional/topic_management_spec.rb
@@ -9,4 +9,17 @@ describe "Topic management API", functional: true do
 
     expect(partitions).to eq 3
   end
+
+  example "deleting topics" do
+    topic = generate_topic_name
+    kafka.create_topic(topic, num_partitions: 3)
+
+    expect(kafka.partitions_for(topic)).to eq 3
+    kafka.delete_topic(topic)
+    kafka.instance_variable_get(:@cluster).mark_as_stale!
+
+    expect do
+      kafka.partitions_for(topic)
+    end.to raise_error(Kafka::LeaderNotAvailable)
+  end
 end
