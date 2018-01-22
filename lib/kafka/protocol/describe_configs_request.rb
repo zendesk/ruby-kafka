57e109c4c6649300ee74a2b0eac0faa8e23ceed1
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-01-23 00:49:30 +0700

    Implement DescribeConfigs requests and responses

---
+++ lib/kafka/protocol/describe_configs_request.rb
@@ -0,0 +1,33 @@
+module Kafka
+  module Protocol
+
+    class DescribeConfigsRequest
+      def initialize(resources:)
+        @resources = resources
+      end
+
+      def api_key
+        DESCRIBE_CONFIGS_API
+      end
+
+      def api_version
+        0
+      end
+
+      def response_class
+        Protocol::DescribeConfigsResponse
+      end
+
+      def encode(encoder)
+        encoder.write_array(@resources) do |type, name, configs|
+          encoder.write_int8(type)
+          encoder.write_string(name)
+          encoder.write_array(configs) do |config|
+            encoder.write_string(config)
+          end
+        end
+      end
+    end
+
+  end
+end
