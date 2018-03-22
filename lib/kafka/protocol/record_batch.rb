a51988821de5951cecefd8d7361fca3db4899966
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-03-22 12:48:40 +0700

    Implement record batch #decode

--- lib/kafka/protocol/record_batch.rb
+++ lib/kafka/protocol/record_batch.rb
@@ -3,23 +3,30 @@ require "kafka/protocol/record"
 module Kafka
   module Protocol
     class RecordBatch
+      MAGIC_BYTE = 2
       # The size of metadata before the real record data
       RECORD_BATCH_OVERHEAD = 49
 
-      attr_reader :records, :partition_leader_epoch, :in_traction, :has_control_message, :last_offset_delta, :max_timestamp, :producer_id, :producer_epoch, :first_sequence
+      attr_reader :records, :first_offset, :partition_leader_epoch, :codec_id, :in_traction, :has_control_message, :last_offset_delta, :max_timestamp, :producer_id, :producer_epoch, :first_sequence
 
       def initialize(
           records: [],
-          partition_leader_epoch:,
+          first_offset: 0,
+          first_timestamp: 0,
+          partition_leader_epoch: 0,
+          codec_id: 0,
           in_traction: false,
           has_control_message: false,
-          last_offset_delta:,
-          producer_id:,
-          producer_epoch:,
-          first_sequence:,
-          max_timestamp:
+          last_offset_delta: 0,
+          producer_id: 0,
+          producer_epoch: 0,
+          first_sequence: 0,
+          max_timestamp: 0
       )
         @records = records
+        @first_offset = first_offset
+        @first_timestamp = first_timestamp
+        @codec_id = codec_id
 
         # Records verification
         @last_offset_delta = last_offset_delta
@@ -33,6 +40,68 @@ module Kafka
         @partition_leader_epoch = partition_leader_epoch
         @in_traction = in_traction
         @has_control_message = has_control_message
+      end
+
+      def attributes
+        0x0000 | @codec_id |
+          (@in_traction ? 0x10000 : 0x0) |
+          (@has_control_message ? 0x100000 : 0x0)
+      end
+
+      def encode(encoder)
+        encoder.write_int64(@first_offset)
+
+        record_batch_buffer = StringIO.new
+        record_batch_encoder = Encoder.new(record_batch_buffer)
+
+        record_batch_encoder.write_int32(@partition_leader_epoch)
+        record_batch_encoder.write_int8(MAGIC_BYTE)
+
+        data = encode_record_batch_body
+        crc = Zlib.crc32(data)
+
+        record_batch_encoder.write_int32(crc)
+        record_batch_encoder.write(data)
+
+        encoder.write_bytes(record_batch_buffer.string)
+      end
+
+      def encode_record_batch_body
+        buffer = StringIO.new
+        encoder = Encoder.new(buffer)
+
+        encoder.write_int16(attributes)
+        encoder.write_int32(@last_offset_delta)
+        encoder.write_int64(@first_timestamp)
+        encoder.write_int64(@max_timestamp)
+
+        encoder.write_int64(@producer_id)
+        encoder.write_int16(@producer_epoch)
+        encoder.write_int32(@first_sequence)
+
+        encoder.write_int32(@records.length)
+
+        records_array = encode_record_array
+        if compressed?
+          codec = Compression.find_codec_by_id(@codec_id)
+          records_array = codec.compress(records_array)
+        end
+        encoder.write(records_array)
+
+        buffer.string
+      end
+
+      def encode_record_array
+        buffer = StringIO.new
+        encoder = Encoder.new(buffer)
+        @records.each do |record|
+          record.encode(encoder)
+        end
+        buffer.string
+      end
+
+      def compressed?
+        @codec_id != 0
       end
 
       def self.decode(decoder)
@@ -82,6 +151,8 @@ module Kafka
 
         new(
           records: records_array,
+          first_offset: first_offset,
+          first_timestamp: first_timestamp,
           partition_leader_epoch: partition_leader_epoch,
           in_traction: in_traction,
           has_control_message: has_control_message,
--- spec/protocol/record_batch_spec.rb
+++ spec/protocol/record_batch_spec.rb
@@ -1,40 +1,275 @@
 require 'spec_helper'
 
 describe Kafka::Protocol::RecordBatch do
-  context '.decode' do
-    let(:decoder) { Kafka::Protocol::Decoder.new(byte_array_to_io(bytes)) }
+  let(:empty_record_batch_bytes) do
+    [
+      # First offset
+      0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+      # Record Batch Length
+      0x0, 0x0, 0x0, 0x31,
+      # Partition Leader Epoch
+      0x0, 0x0, 0x0, 0x0,
+      # Magic byte
+      0x2,
+      # CRC
+      0xe9, 0xec, 0x3d, 0xb1,
+      # Attributes
+      0x0, 0x0,
+      # Last offset delta
+      0x0, 0x0, 0x0, 0x0,
+      # First timestamp
+      0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+      # Max timestamp
+      0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+      # Producer ID
+      0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+      # Producer epoch
+      0x0, 0x0,
+      # First sequence
+      0x0, 0x0, 0x0, 0x0,
+      # Number of records
+      0x0, 0x0, 0x0, 0x0
+    ]
+  end
+
+  let(:sample_record_batch) do
+    Kafka::Protocol::RecordBatch.new(
+      first_offset: 1,
+      first_timestamp: 1521656000000,
+      partition_leader_epoch: 2,
+      codec_id: codec_id,
+      has_control_message: false,
+      last_offset_delta: 3,
+      max_timestamp: 1521658000000,
+      producer_id: 123456,
+      producer_epoch: 2,
+      first_sequence: 0,
+      records: [
+        Kafka::Protocol::Record.new(
+          attributes:     1,
+          timestamp_delta: 1000,
+          offset_delta:    1,
+          key: 'hello',
+          value: 'world',
+          headers: {
+            'a' => '12'
+          }
+        ),
+        Kafka::Protocol::Record.new(
+          attributes: 2,
+          timestamp_delta: 2000,
+          offset_delta: 2,
+          key: 'ruby',
+          value: 'kafka',
+          headers: {
+            'b' => '34'
+          }
+        )
+      ]
+    )
+  end
+
+  let(:sample_record_batch_metadata_bytes) {
+    [
+      # Last offset delta
+      0x0, 0x0, 0x0, 0x3,
+      # First timestamp
+      0x0, 0x0, 0x1, 0x62, 0x49, 0xc3, 0xee, 0x0,
+      # Max timestamp
+      0x0, 0x0, 0x1, 0x62, 0x49, 0xe2, 0x72, 0x80,
+      # Producer ID
+      0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0xe2, 0x40,
+      # Producer epoch
+      0x0, 0x2,
+      # First sequence
+      0x0, 0x0, 0x0, 0x0,
+      # Number of records
+      0x0, 0x0, 0x0, 0x2
+    ]
+  }
+
+  let(:record_1_bytes) {
+    [
+      # Size
+      0x2c,
+      # Attributes
+      0x1,
+      # Timestamp delta
+      0xd0, 0xf,
+      # Offset delta
+      0x2,
+      # Key
+      0xa,
+      0x68, 0x65, 0x6c, 0x6c, 0x6f,
+      # Value
+      0xa,
+      0x77, 0x6f, 0x72, 0x6c, 0x64,
+      # Header
+      0x2,
+      0x2, 0x61,
+      0x4, 0x31, 0x32,
+    ]
+  }
+
+  let(:record_2_bytes) {
+    [
+      # Size
+      0x2a,
+      # Attributes
+      0x2,
+      # Timestamp delta
+      0xa0, 0x1f,
+      # Offset delta
+      0x4,
+      # Key
+      0x8,
+      0x72, 0x75, 0x62, 0x79,
+      # Value
+      0xa,
+      0x6b, 0x61, 0x66, 0x6b, 0x61,
+      # Header
+      0x2,
+      0x2, 0x62,
+      0x4, 0x33, 0x34
+    ]
+  }
+
+  let(:sample_record_batch_uncompressed_bytes) do
+    [
+      # First offset
+      0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1,
+      # Record Batch Length
+      0x0, 0x0, 0x0, 0x5e,
+      # Partition Leader Epoch
+      0x0, 0x0, 0x0, 0x2,
+      # Magic byte
+      0x2,
+      # CRC
+      0x0, 0x0, 0x0, 0x0,
+      # Attributes
+      0x0, 0x0,
+      sample_record_batch_metadata_bytes,
+      record_1_bytes,
+      record_2_bytes
+    ].flatten
+  end
+
+  let(:sample_record_batch_gzip_bytes) do
+    [
+      # First offset
+      0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1,
+      # Record Batch Length
+      0x0, 0x0, 0x0, 0x72,
+      # Partition Leader Epoch
+      0x0, 0x0, 0x0, 0x2,
+      # Magic byte
+      0x2,
+      # CRC
+      0x0, 0x0, 0x0, 0x0,
+      # Attributes
+      0x0, 0x1,
+      sample_record_batch_metadata_bytes,
+      Kafka::GzipCodec.new.compress(
+        (record_1_bytes + record_2_bytes).pack("C*")
+      ).bytes
+    ].flatten
+  end
+
+  let(:sample_record_batch_snappy_bytes) do
+    [
+      # First offset
+      0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1,
+      # Record Batch Length
+      0x0, 0x0, 0x0, 0x60,
+      # Partition Leader Epoch
+      0x0, 0x0, 0x0, 0x2,
+      # Magic byte
+      0x2,
+      # CRC
+      0x0, 0x0, 0x0, 0x0,
+      # Attributes
+      0x0, 0x2,
+      sample_record_batch_metadata_bytes,
+      Kafka::SnappyCodec.new.compress(
+        (record_1_bytes + record_2_bytes).pack("C*")
+      ).bytes
+    ].flatten
+  end
+
+  let(:sample_record_batch_lz4_bytes) do
+    [
+      # First offset
+      0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1,
+      # Record Batch Length
+      0x0, 0x0, 0x0, 0x71,
+      # Partition Leader Epoch
+      0x0, 0x0, 0x0, 0x2,
+      # Magic byte
+      0x2,
+      # CRC
+      0x0, 0x0, 0x0, 0x0,
+      # Attributes
+      0x0, 0x3,
+      sample_record_batch_metadata_bytes,
+      Kafka::LZ4Codec.new.compress(
+        (record_1_bytes + record_2_bytes).pack("C*")
+      ).bytes
+    ].flatten
+  end
+
+  context '#encode' do
+    let(:buffer) { StringIO.new }
+    let(:encoder) { Kafka::Protocol::Encoder.new(buffer) }
 
     context 'Empty record batch' do
-      let(:bytes) do
-        [
-          # First offset
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-          # Record Batch Length
-          0x0, 0x0, 0x0, 0x31,
-          # Partition Leader Epoch
-          0x0, 0x0, 0x0, 0x0,
-          # Magic byte
-          0x2,
-          # CRC
-          0x59, 0x5f, 0xb7, 0xdd,
-          # Attributes
-          0x0, 0x0,
-          # Last offset delta
-          0x0, 0x0, 0x0, 0x0,
-          # First timestamp
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-          # Max timestamp
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-          # Producer ID
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-          # Producer epoch
-          0x0, 0x0,
-          # First sequence
-          0x0, 0x0, 0x0, 0x0,
-          # Number of records
-          0x0, 0x0, 0x0, 0x0
-        ]
+      let(:record_batch) { Kafka::Protocol::RecordBatch.new }
+
+      it 'encodes an empty batch with no records' do
+        record_batch.encode(encoder)
+        expect(buffer.string.bytes).to eql empty_record_batch_bytes
       end
+    end
+
+    context 'Uncompressed record batch' do
+      let(:codec_id) { 0 }
+
+      it 'encodes the record batch without compressing' do
+        sample_record_batch.encode(encoder)
+        expect(strip_crc(buffer.string.bytes)).to eql sample_record_batch_uncompressed_bytes
+      end
+    end
+
+    context 'Compress with GZIP' do
+      let(:codec_id) { 1 }
+
+      it 'encodes the record batch using gzip compressor' do
+        sample_record_batch.encode(encoder)
+        expect(strip_crc(buffer.string.bytes)).to eql sample_record_batch_gzip_bytes
+      end
+    end
+
+    context 'Compress with Snappy' do
+      let(:codec_id) { 2 }
+
+      it 'encodes the record batch using snappy compressor' do
+        sample_record_batch.encode(encoder)
+        expect(strip_crc(buffer.string.bytes)).to eql sample_record_batch_snappy_bytes
+      end
+    end
+
+    context 'Compress with LZ4' do
+      let(:codec_id) { 3 }
+
+      it 'encodes the record batch using snappy compressor' do
+        sample_record_batch.encode(encoder)
+        expect(strip_crc(buffer.string.bytes)).to eql sample_record_batch_lz4_bytes
+      end
+    end
+  end
+
+  context '.decode' do
+    context 'Empty record batch' do
+      let(:decoder) { Kafka::Protocol::Decoder.new(byte_array_to_io(empty_record_batch_bytes)) }
 
       it 'returns record batch with no records' do
         record_batch = Kafka::Protocol::RecordBatch.decode(decoder)
@@ -55,90 +290,9 @@ describe Kafka::Protocol::RecordBatch do
       end
     end
 
-    let(:batch_metadata) {
-      [
-        # Last offset delta
-        0x0, 0x0, 0x0, 0x3,
-        # First timestamp
-        0x0, 0x0, 0x1, 0x62, 0x49, 0xc3, 0xee, 0x0,
-        # Max timestamp
-        0x0, 0x0, 0x1, 0x62, 0x49, 0xe2, 0x72, 0x80,
-        # Producer ID
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0xe2, 0x40,
-        # Producer epoch
-        0x0, 0x2,
-        # First sequence
-        0x0, 0x0, 0x0, 0x0,
-        # Number of records
-        0x0, 0x0, 0x0, 0x2
-      ]
-    }
-
-    let(:record_1) {
-      [
-        # Size
-        0x2c,
-        # Attributes
-        0x1,
-        # Timestamp delta
-        0xd0, 0xf,
-        # Offset delta
-        0x2,
-        # Key
-        0xa,
-        0x68, 0x65, 0x6c, 0x6c, 0x6f,
-        # Value
-        0xa,
-        0x77, 0x6f, 0x72, 0x6c, 0x64,
-        # Header
-        0x2,
-        0x2, 0x61,
-        0x4, 0x31, 0x32,
-      ]
-    }
-
-    let(:record_2) {
-      [
-        # Size
-        0x2a,
-        # Attributes
-        0x2,
-        # Timestamp delta
-        0xa0, 0x1f,
-        # Offset delta
-        0x4,
-        # Key
-        0x8,
-        0x72, 0x75, 0x62, 0x79,
-        # Value
-        0xa,
-        0x6b, 0x61, 0x66, 0x6b, 0x61,
-        # Header
-        0x2,
-        0x2, 0x62,
-        0x4, 0x33, 0x34
-      ]
-    }
-
     context 'Uncompressed records' do
-      let(:bytes) do
-        [
-          # First offset
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1,
-          # Record Batch Length
-          0x0, 0x0, 0x0, 0x5e,
-          # Partition Leader Epoch
-          0x0, 0x0, 0x0, 0x2,
-          # Magic byte
-          0x2,
-          # CRC
-          0x99, 0x52, 0xcb, 0xd8,
-          # Attributes
-          0x0, 0x0,
-          batch_metadata,
-          record_1,
-          record_2
-        ].flatten
+      let(:decoder) do
+        Kafka::Protocol::Decoder.new(byte_array_to_io(sample_record_batch_uncompressed_bytes))
       end
 
       it 'decodes records without decompressing' do
@@ -149,57 +303,23 @@ describe Kafka::Protocol::RecordBatch do
     end
 
     context 'Compress with GZIP' do
-      let(:bytes) do
-        [
-          # First offset
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1,
-          # Record Batch Length
-          0x0, 0x0, 0x0, 0x72,
-          # Partition Leader Epoch
-          0x0, 0x0, 0x0, 0x2,
-          # Magic byte
-          0x2,
-          # CRC
-          0x71, 0xb3, 0xf, 0x80,
-          # Attributes
-          0x0, 0x1,
-          batch_metadata,
-          Kafka::GzipCodec.new.compress(
-            (record_1 + record_2).pack("C*")
-          ).bytes
-        ].flatten
+      let(:decoder) do
+        Kafka::Protocol::Decoder.new(byte_array_to_io(sample_record_batch_gzip_bytes))
       end
 
-      it 'decodes records without decompressing' do
+      it 'decodes records with GZIP decompressor' do
         record_batch = Kafka::Protocol::RecordBatch.decode(decoder)
         expect_matched_batch_metadata(record_batch)
         expect_matched_records(record_batch.records)
       end
     end
 
-    context 'Compress with GZIP' do
-      let(:bytes) do
-        [
-          # First offset
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1,
-          # Record Batch Length
-          0x0, 0x0, 0x0, 0x60,
-          # Partition Leader Epoch
-          0x0, 0x0, 0x0, 0x2,
-          # Magic byte
-          0x2,
-          # CRC
-          0x71, 0xb3, 0xf, 0x80,
-          # Attributes
-          0x0, 0x2,
-          batch_metadata,
-          Kafka::SnappyCodec.new.compress(
-            (record_1 + record_2).pack("C*")
-          ).bytes
-        ].flatten
+    context 'Compress with Snappy' do
+      let(:decoder) do
+        Kafka::Protocol::Decoder.new(byte_array_to_io(sample_record_batch_snappy_bytes))
       end
 
-      it 'decodes records without decompressing' do
+      it 'decodes records with Snappy decompressor' do
         record_batch = Kafka::Protocol::RecordBatch.decode(decoder)
         expect_matched_batch_metadata(record_batch)
         expect_matched_records(record_batch.records)
@@ -207,28 +327,11 @@ describe Kafka::Protocol::RecordBatch do
     end
 
     context 'Compress with LZ4' do
-      let(:bytes) do
-        [
-          # First offset
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1,
-          # Record Batch Length
-          0x0, 0x0, 0x0, 0x71,
-          # Partition Leader Epoch
-          0x0, 0x0, 0x0, 0x2,
-          # Magic byte
-          0x2,
-          # CRC
-          0x71, 0xb3, 0xf, 0x80,
-          # Attributes
-          0x0, 0x3,
-          batch_metadata,
-          Kafka::LZ4Codec.new.compress(
-            (record_1 + record_2).pack("C*")
-          ).bytes
-        ].flatten
+      let(:decoder) do
+        Kafka::Protocol::Decoder.new(byte_array_to_io(sample_record_batch_lz4_bytes))
       end
 
-      it 'decodes records without decompressing' do
+      it 'decodes records with LZ4 decompressor' do
         record_batch = Kafka::Protocol::RecordBatch.decode(decoder)
         expect_matched_batch_metadata(record_batch)
         expect_matched_records(record_batch.records)
@@ -283,4 +386,14 @@ def expect_matched_records(records)
   expect(record_2.key).to eql('ruby')
   expect(record_2.value).to eql('kafka')
   expect(record_2.headers).to eql('b' => '34')
+end
+
+# Compression usually has random factors. Therefore, the content and crc change
+# all the time. It is useless to compare crc
+def strip_crc(bytes)
+  bytes[17] = 0x0
+  bytes[18] = 0x0
+  bytes[19] = 0x0
+  bytes[20] = 0x0
+  bytes
 end
