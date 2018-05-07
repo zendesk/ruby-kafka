c09fa4612f08abad03f87345875834edaefc7a8e
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-05-07 10:52:31 +0200

    Clean up the docs a bit

--- lib/kafka/ssl_socket_with_timeout.rb
+++ lib/kafka/ssl_socket_with_timeout.rb
@@ -94,10 +94,10 @@ module Kafka
 
       until buffer.length >= num_bytes
         begin
-          # unlike plain tcp sockets, ssl sockets don't support IO.select
+          # Unlike plain TCP sockets, SSL sockets don't support IO.select
           # properly.
           # Instead, timeouts happen on a per read basis, and we have to
-          # catch exceptions from read_nonblock, and gradually build up
+          # catch exceptions from read_nonblock and gradually build up
           # our read buffer.
           buffer << @ssl_socket.read_nonblock(num_bytes - buffer.length)
         rescue IO::WaitReadable
@@ -114,6 +114,7 @@ module Kafka
           end
         end
       end
+
       buffer
     end
 
