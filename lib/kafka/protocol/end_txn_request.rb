fcb430de66cf5376c2a6861f963865fa55ba4f01
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-06-29 19:57:32 +0700

    Fix missing transactional result

--- lib/kafka/protocol/end_txn_request.rb
+++ lib/kafka/protocol/end_txn_request.rb
@@ -7,7 +7,7 @@ module Kafka
         @transactional_id = transactional_id
         @producer_id = producer_id
         @producer_epoch = producer_epoch
-        @transaction_result
+        @transaction_result = transaction_result
       end
 
       def api_key
@@ -19,7 +19,7 @@ module Kafka
       end
 
       def encode(encoder)
-        encoder.write_string(@transactional_id.to_s)
+        encoder.write_string(@transactional_id)
         encoder.write_int64(@producer_id)
         encoder.write_int16(@producer_epoch)
         encoder.write_boolean(@transaction_result)
