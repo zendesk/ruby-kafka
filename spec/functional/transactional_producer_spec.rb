2bd62260b5ee0e46a687f4bff48c0de389ce97ed
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-07-02 23:57:26 +0700

    Provide more functional tests for transactions

--- spec/functional/transactional_producer_spec.rb
+++ spec/functional/transactional_producer_spec.rb
@@ -118,6 +118,49 @@ describe "Transactional producer", functional: true do
     producer.shutdown
   end
 
+  example 'Multi-topic transaction' do
+    producer = kafka.producer(
+      transactional: true,
+      transactional_id: SecureRandom.uuid
+    )
+
+    topic_1 = create_random_topic(num_partitions: 3)
+    topic_2 = create_random_topic(num_partitions: 3)
+
+    producer.init_transactions
+    producer.begin_transaction
+    producer.produce('Test 1', topic: topic_1, partition: 0)
+    producer.produce('Test 2', topic: topic_1, partition: 1)
+    producer.deliver_messages
+    producer.produce('Test 3', topic: topic_2, partition: 0)
+    producer.produce('Test 4', topic: topic_2, partition: 1)
+    producer.produce('Test 5', topic: topic_2, partition: 2)
+    producer.deliver_messages
+    producer.commit_transaction
+
+    records = kafka.fetch_messages(topic: topic_1, partition: 0, offset: :earliest)
+    expect(records.length).to eql(1)
+    expect(records[0].value).to eql('Test 1')
+
+    records = kafka.fetch_messages(topic: topic_1, partition: 1, offset: :earliest)
+    expect(records.length).to eql(1)
+    expect(records[0].value).to eql('Test 2')
+
+    records = kafka.fetch_messages(topic: topic_2, partition: 0, offset: :earliest)
+    expect(records.length).to eql(1)
+    expect(records[0].value).to eql('Test 3')
+
+    records = kafka.fetch_messages(topic: topic_2, partition: 1, offset: :earliest)
+    expect(records.length).to eql(1)
+    expect(records[0].value).to eql('Test 4')
+
+    records = kafka.fetch_messages(topic: topic_2, partition: 2, offset: :earliest)
+    expect(records.length).to eql(1)
+    expect(records[0].value).to eql('Test 5')
+
+    producer.shutdown
+  end
+
   example 'Consumer could not read aborted transactional production' do
     producer = kafka.producer(
       transactional: true,
@@ -141,6 +184,44 @@ describe "Transactional producer", functional: true do
     expect(records.length).to eql(0)
 
     records = kafka.fetch_messages(topic: topic, partition: 2, offset: :earliest, max_wait_time: 1)
+    expect(records.length).to eql(0)
+
+    producer.shutdown
+  end
+
+  example 'Multi-topic aborted transactions' do
+    producer = kafka.producer(
+      transactional: true,
+      transactional_id: SecureRandom.uuid
+    )
+
+    topic_1 = create_random_topic(num_partitions: 3)
+    topic_2 = create_random_topic(num_partitions: 3)
+
+    producer.init_transactions
+    producer.begin_transaction
+    producer.produce('Test 1', topic: topic_1, partition: 0)
+    producer.produce('Test 2', topic: topic_1, partition: 1)
+    producer.deliver_messages
+    producer.produce('Test 3', topic: topic_2, partition: 0)
+    producer.produce('Test 4', topic: topic_2, partition: 1)
+    producer.produce('Test 5', topic: topic_2, partition: 2)
+    producer.deliver_messages
+    producer.abort_transaction
+
+    records = kafka.fetch_messages(topic: topic_1, partition: 0, offset: :earliest)
+    expect(records.length).to eql(0)
+
+    records = kafka.fetch_messages(topic: topic_1, partition: 1, offset: :earliest)
+    expect(records.length).to eql(0)
+
+    records = kafka.fetch_messages(topic: topic_2, partition: 0, offset: :earliest)
+    expect(records.length).to eql(0)
+
+    records = kafka.fetch_messages(topic: topic_2, partition: 1, offset: :earliest)
+    expect(records.length).to eql(0)
+
+    records = kafka.fetch_messages(topic: topic_2, partition: 2, offset: :earliest)
     expect(records.length).to eql(0)
 
     producer.shutdown
@@ -208,8 +289,10 @@ describe "Transactional producer", functional: true do
       producer_2.init_transactions
     end.to raise_error(Kafka::ConcurrentTransactionError)
 
-    producer_1.shutdown
-    producer_2.shutdown
+    begin
+      producer_1.shutdown
+      producer_2.shutdown
+    rescue; end
   end
 
   example 'with_transaction syntax' do
@@ -368,5 +451,71 @@ describe "Transactional producer", functional: true do
     expect(records.length).to eql(0)
 
     producer.shutdown
+  end
+
+  example 'Client excludes aborted messages' do
+    topic = create_random_topic(num_partitions: 3)
+
+    producer_1 = kafka.producer(
+      transactional: true,
+      transactional_id: SecureRandom.uuid
+    )
+    producer_2 = kafka.producer(
+      transactional: true,
+      transactional_id: SecureRandom.uuid
+    )
+
+    producer_1.init_transactions
+    producer_2.init_transactions
+
+    producer_1.begin_transaction
+    producer_1.produce('Test 1', topic: topic, partition: 0)
+    producer_1.produce('Test 2', topic: topic, partition: 1)
+    producer_1.deliver_messages
+
+    producer_2.begin_transaction
+    producer_2.produce('Test 3', topic: topic, partition: 0)
+    producer_2.produce('Test 4', topic: topic, partition: 1)
+    producer_2.produce('Test 5', topic: topic, partition: 2)
+    producer_2.deliver_messages
+
+    producer_1.commit_transaction
+    producer_2.commit_transaction
+
+    producer_1.begin_transaction
+    producer_1.produce('Test 6', topic: topic, partition: 0)
+    producer_1.produce('Test 7', topic: topic, partition: 1)
+    producer_1.deliver_messages
+    producer_1.commit_transaction
+
+    producer_2.begin_transaction
+    producer_2.produce('Test 8', topic: topic, partition: 0)
+    producer_2.produce('Test 9', topic: topic, partition: 1)
+    producer_2.produce('Test 10', topic: topic, partition: 2)
+    producer_2.deliver_messages
+    producer_2.abort_transaction
+
+    producer_1.begin_transaction
+    producer_1.produce('Test 11', topic: topic, partition: 0)
+    producer_1.produce('Test 12', topic: topic, partition: 1)
+    producer_1.deliver_messages
+    producer_1.commit_transaction
+
+    sleep 1
+
+    records = kafka.fetch_messages(topic: topic, partition: 0, offset: :earliest)
+    expect(records.length).to eql(4)
+    expect(records.map(&:value)).to match_array(['Test 1', 'Test 3', 'Test 6', 'Test 11'])
+
+    records = kafka.fetch_messages(topic: topic, partition: 1, offset: :earliest)
+    expect(records.length).to eql(4)
+    expect(records.map(&:value)).to match_array(['Test 2', 'Test 4', 'Test 7', 'Test 12'])
+
+    records = kafka.fetch_messages(topic: topic, partition: 2, offset: :earliest)
+    expect(records.length).to eql(1)
+    expect(records.map(&:value)).to eql(['Test 5'])
+
+    producer_1.shutdown
+    producer_2.shutdown
   end
 end
