434db63fae8dd2705b01bd473947bfeafd0676a5
Phong Pham <phong.pham@shopify.com>
2019-02-19 14:11:39 -0500

    Initial commit containing all unit tests and PR feedback from the same PR but with a different fork

---
+++ spec/fake_token_provider.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class FakeTokenProvider
+  def token
+    "SASLOAUTHBEARER.TEST_ID_TOKEN"
+  end
+
+  def extensions
+    { test_key: "test_value", test_key_2: "test_value_2" }
+  end
+end
+
+class FakeBrokenTokenProvider
+end
