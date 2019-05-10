5326e987516acc83094ec1814cd8b9b17245cd9e
Daniel Orner <daniel.orner@wishabi.com>
2019-05-10 17:01:37 -0400

    Change TaggedLogger to use a class implementation

--- lib/kafka/tagged_logger.rb
+++ lib/kafka/tagged_logger.rb
@@ -4,10 +4,14 @@ require 'forwardable'
 # ActiveSupport::TaggedLogging.
 
 module Kafka
-  module TaggedFormatter
+  class TaggedFormatter
+
+    def initialize(formatter)
+      @formatter = formatter
+    end
 
     def call(severity, timestamp, progname, msg)
-      super(severity, timestamp, progname, "#{tags_text}#{msg}")
+      @formatter.call(severity, timestamp, progname, "#{tags_text}#{msg}")
     end
 
     def tagged(*tags)
@@ -46,17 +50,38 @@ module Kafka
 
   end
 
-  module TaggedLogger
+  class TaggedLogger
     extend Forwardable
     delegate [:push_tags, :pop_tags, :clear_tags!] => :formatter
+    delegate [:info, :error, :warn, :fatal, :debug, :level, :level=, :progname,
+              :datetime_format=, :datetime_format, :sev_threshold,
+              :sev_threshold=, :formatter, :debug?, :info?,
+              :warn?, :error?, :fatal?, :reopen, :add, :log, :<<,
+              :unknown, :close] => :@logger
 
-    def self.new(logger)
-      logger ||= Logger.new(nil)
-      return logger if logger.respond_to?(:push_tags) # already included
-      # Ensure we set a default formatter so we aren't extending nil!
-      logger.formatter ||= Logger::Formatter.new
-      logger.formatter.extend TaggedFormatter
-      logger.extend(self)
+    def formatter=(formatter)
+      @logger.formatter = if formatter.is_a?(TaggedFormatter)
+                         formatter
+                        else
+                          TaggedFormatter.new(formatter || Logger::Formatter.new)
+                       end
+    end
+
+    def self.new(logger_or_stream=nil)
+      # don't keep wrapping the same logger over and over again
+      return logger_or_stream if logger_or_stream.is_a?(TaggedLogger)
+      super
+    end
+
+    def initialize(logger_or_stream=nil)
+      @logger = if logger_or_stream.is_a?(Logger)
+                  logger_or_stream.clone
+                elsif logger_or_stream
+                  Logger.new(logger_or_stream)
+                else
+                  Logger.new(nil)
+                end
+      self.formatter = @logger.formatter
     end
 
     def tagged(*tags)
