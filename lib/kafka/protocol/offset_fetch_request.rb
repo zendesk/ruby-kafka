4d2fa43ea56a896876ceb3ae7e379cf5806fc607
Ryan Krage <krage.ryan@gmail.com>
2019-12-09 17:51:28 +0000

    add comment about api version

--- lib/kafka/protocol/offset_fetch_request.rb
+++ lib/kafka/protocol/offset_fetch_request.rb
@@ -12,6 +12,8 @@ module Kafka
         OFFSET_FETCH_API
       end
 
+      # setting topics to nil fetches all offsets for a consumer group
+      # and that feature is only available in API version 2+
       def api_version
         @topics.nil? ? 2 : 1
       end
