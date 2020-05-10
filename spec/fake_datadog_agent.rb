423629abcdf3381813d8264856d1cd0be48afa80
abicky <takeshi.arabiki@gmail.com>
2020-05-11 03:14:31 +0900

    Support Unix domain socket in Kafka::Datadog
    dogstatsd-ruby has supported Unix domain socket.
    cf. https://github.com/DataDog/dogstatsd-ruby/pull/61

--- spec/fake_datadog_agent.rb
+++ spec/fake_datadog_agent.rb
@@ -1,27 +1,39 @@
 # frozen_string_literal: true
 
 require "socket"
+require "tempfile"
 
 class FakeDatadogAgent
-  attr_reader :host, :port, :metrics
+  attr_reader :host, :port, :socket_path, :metrics
 
   def initialize
     @host = "127.0.0.1"
     @port = 9999
-    @socket = UDPSocket.new
-    @thread = nil
+    @socket_path = Tempfile.open("fake_datadog_agent_sock") do |f|
+      path = f.path
+      f.unlink
+      path
+    end
+    @udp_socket = UDPSocket.new
+    @uds_socket = Socket.new(Socket::AF_UNIX, Socket::SOCK_DGRAM)
+    @threads = []
     @metrics = []
-
-    @socket.bind(@host, @port)
   end
 
   def start
-    @thread = Thread.new { loop { receive } }
-    @thread.abort_on_exception = true
+    @udp_socket.bind(@host, @port)
+    @uds_socket.bind(Addrinfo.unix(@socket_path))
+
+    @threads << Thread.new { loop { receive_from_udp_socket } }
+    @threads << Thread.new { loop { receive_from_uds_socket } }
+    @threads.each { |th| th.abort_on_exception = true }
   end
 
   def stop
-    @thread.kill
+    @threads.each(&:kill)
+    @udp_socket.close
+    @uds_socket.close
+    @threads = []
   end
 
   def wait_for_metrics(count: 1)
@@ -34,9 +46,17 @@ class FakeDatadogAgent
 
   private
 
-  def receive
-    data, sender = @socket.recvfrom(512)
+  def receive_from_udp_socket
+    data, _ = @udp_socket.recvfrom(512)
+    add_metrics(data)
+  end
 
+  def receive_from_uds_socket
+    data, _ = @uds_socket.recvfrom(512)
+    add_metrics(data)
+  end
+
+  def add_metrics(data)
     data.split("\n").each do |message|
       metric = message.split(":").first
       @metrics << metric if metric
