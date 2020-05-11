07faba21d99020774f14dbb2e8a574afcd136b04
lihightower <lairenhightower@gmail.com>
2020-05-11 14:51:45 -0700

    Fixup verify_hostname for ssl contexts

--- lib/kafka/ssl_context.rb
+++ lib/kafka/ssl_context.rb
@@ -54,10 +54,11 @@ module Kafka
           store.set_default_paths
         end
         ssl_context.cert_store = store
-        ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        # Verify certificate hostname if supported (ruby >= 2.4.0)
-        ssl_context.verify_hostname = verify_hostname if ssl_context.respond_to?(:verify_hostname=)
       end
+
+      ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      # Verify certificate hostname if supported (ruby >= 2.4.0)
+      ssl_context.verify_hostname = verify_hostname if ssl_context.respond_to?(:verify_hostname=)
 
       ssl_context
     end
